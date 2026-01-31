345d215da48c927ef4fe4f63b50f7a30c1bf83bd
Alesia <alesia.litvinenko@gmail.com>
2026-01-31 21:41:15 +0300

    Implement layout for auth pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default function SignIn() {
+  return (
+    <div className="flex min-h-screen  home-wrapper">
+      <section className="grid w-full gap-8 home-section">SignIn</section>
+    </div>
+  )
+}
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <div className="flex min-h-screen  home-wrapper">
+      <section className="grid w-full gap-8 home-section">SignUp</section>
+    </div>
+  )
+}
